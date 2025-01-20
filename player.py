a79e27c204edaa3017f25c7b8141a730e3597ce5
Jeffrey Sing <jeffreysing1188@gmail.com>
2025-01-20 16:27:37 +0800

    add player shooting ability

--- player.py
+++ player.py
@@ -1,17 +1,20 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SHOOT_COOLDOWN, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED
+from shot import Shot
 
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -23,6 +26,9 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+
+        if keys[pygame.K_SPACE]:
+            self.shoot()
      
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -38,3 +44,14 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        shot_start_pos = self.position + forward * (self.radius)
+
+        shot = Shot(shot_start_pos.x, shot_start_pos.y)
+        shot.velocity = forward * PLAYER_SHOOT_SPEED

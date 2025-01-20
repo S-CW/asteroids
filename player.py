1da7840bd3bb0d8583e6dbe9b5ad7004576760c0
Jeffrey Sing <jeffreysing1188@gmail.com>
2025-01-20 16:55:37 +0800

    fix shots not align to triangle tip

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_SHOOT_COOLDOWN, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SHOOT_COOLDOWN, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED, SHOT_RADIUS
 from shot import Shot
 
 
@@ -17,14 +17,14 @@ class Player(CircleShape):
         self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
             
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.move(dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.move(-dt)
 
         if keys[pygame.K_SPACE]:
@@ -32,11 +32,14 @@ class Player(CircleShape):
      
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
-        return [a, b, c]
+        right = pygame.Vector2(1, 0).rotate(self.rotation) * self.radius / 1.5
+
+        # define the vertices
+        tip = self.position + forward * self.radius
+        base_left = self.position - forward * (self.radius / 2) + right
+        base_right = self.position - forward * (self.radius / 2) - right
+
+        return [tip, base_left, base_right]
     
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -51,7 +54,7 @@ class Player(CircleShape):
         
         self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot_start_pos = self.position + forward * (self.radius)
+        shot_start_pos = self.position + forward * (self.radius + SHOT_RADIUS)
 
         shot = Shot(shot_start_pos.x, shot_start_pos.y)
         shot.velocity = forward * PLAYER_SHOOT_SPEED
